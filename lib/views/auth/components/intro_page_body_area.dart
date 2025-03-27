cc10fc5c9f48031b8da08d08365eb200e11b90d2
marcusmi4n <marcuslutas@gmail.com>
2025-03-27 10:38:10 +0300

    commented

--- lib/views/auth/components/intro_page_body_area.dart
+++ lib/views/auth/components/intro_page_body_area.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../core/constants/app_colors.dart';
+//import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_defaults.dart';
 import '../../../core/routes/app_routes.dart';
 
